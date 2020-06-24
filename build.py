661f01ddff3315a2559c4876e10b3cfdc58ea9c8
J Law <lawton27@gmail.com>
2020-06-24 20:02:51 +0100

    Build with console open so it is clear when app is loading

--- build.py
+++ build.py
@@ -6,5 +6,5 @@ import subprocess
 # subprocess.call(["auto-py-to-exe", "main.py"])
 
 # Main call for build
-call = "pyinstaller --noconfirm --onefile --windowed --hidden-import \"pkg_resources.py2_warn\"  \"{}\main.py\""
+call = "pyinstaller --noconfirm --onefile --console --hidden-import \"pkg_resources.py2_warn\"  \"{}\main.py\""
 subprocess.call(call.format(os.getcwd()))
